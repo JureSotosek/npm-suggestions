3e61f60c233cb60931c0d83a173593b709ba3166
Jure Sotosek <jure.sotosek@gmail.com>
2018-07-22 15:12:15 +0200

    Added objectID

--- src/config.js
+++ src/config.js
@@ -5,7 +5,5 @@ module.exports = {
   elasticsearchEndpoint: process.env.ELASTICSEARCH_ENDPOINT,
   user: process.env.ELASTICSEARCH_USER,
   password: process.env.ELASTICSEARCH_PASSWORD,
-  indexName: 'npm-registry',
-
-  npmRegistryEndpoint: 'https://replicate.npmjs.com/registry'
+  indexName: 'npm-registry'
 };
