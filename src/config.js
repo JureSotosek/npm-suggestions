1a892accdf40115c0f1ae815829bad65743e8a0f
Jure Sotosek <jure.sotosek@gmail.com>
2018-07-24 21:20:44 +0200

    Changed config for consistency

--- src/config.js
+++ src/config.js
@@ -1,9 +1,25 @@
 const { config } = require('dotenv');
 config();
 
-module.exports = {
-  elasticsearchEndpoint: process.env.ELASTICSEARCH_ENDPOINT,
-  user: process.env.ELASTICSEARCH_USER,
-  password: process.env.ELASTICSEARCH_PASSWORD,
+const defaultConfig = {
+  elasticsearchEndpoint:
+    'https://randomnumbersandletters.us-east-1.aws.found.io:9243/',
+  user: 'elastic',
+  password: '',
   indexName: 'npm-registry'
 };
+
+module.exports = Object.entries(defaultConfig).reduce(
+  (res, [key, defaultValue]) => ({
+    ...res,
+    [key]:
+      key in process.env
+        ? JSON.parse(
+            typeof defaultValue === 'string'
+              ? `"${process.env[key]}"`
+              : process.env[key]
+          )
+        : defaultValue
+  }),
+  {}
+);
